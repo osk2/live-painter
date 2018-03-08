ee1ad3d2017d4f1df5e20d9eb43883c53ed883d7
osk2 <sc60714@gmail.com>
2018-03-08 15:52:02 +0800

    Add ability to control webduino board
    - remove unused code

--- index.js
+++ index.js
@@ -5,33 +5,54 @@ const app = new Vue({
       row: 1,
       column: 1,
       color: '#ffffff',
-      idMode: false,
+      showId: false,
+      showBoardSetting: false,
+      selectMode: '全選',
+      deviceID: '10EdaPvQ',
+      deviceType: 'Smart',
+      pin: 15
     },
     leds: [{
       id: 1,
-      color: '#ffffff',
-      active: false
+      color: '#ffffff'
     }],
     exportedCode: '',
-    constant: {
-      all: '全選',
-      none: '全不選'
-    },
+    webduino: {
+      board: null,
+      queue: null,
+      ws2812: null
+    }
   },
   methods: {
     createLeds() {
+      if (typeof this.input.row !== 'number' || this.input.row < 1) {
+        this.input.row = 1;
+      }
+      if (typeof this.input.column !== 'number' || this.input.column < 1) {
+        this.input.column = 1;
+      }
       const ledCount = this.input.row * this.input.column;
 
       this.leds = [];
       for (let i = 1; i <= ledCount; i++) {
         this.leds.push({
           id: i,
-          color: '#ffffff',
-          active: false
+          color: '#ffffff'
         });
       }
-      this.input.selectMode = this.constant.all;
       this.exportCode();
+    },
+    createBoard(config) {
+      return new Promise((resolve, reject) => {
+        if (!config || !config.board) {
+          reject(new Error('Invalid config'));
+        }
+        const board = new webduino.WebArduino(config);
+        board.once(webduino.BoardEvent.READY, () => {
+          this.webduino.board = board;
+          resolve(board);
+        });
+      });
     },
     triggerColorPicker(e) {
       e.stopPropagation();
@@ -57,6 +78,47 @@ const app = new Vue({
       });
 
       this.exportedCode = codeArray.join('');
+      this.executeCode();
+    },
+    toggleBoardSetting() {
+      this.showBoardSetting = !this.showBoardSetting;
+      if (!this.showBoardSetting) {
+        this.webduino.board.disconnect();
+        this.webduino.board = null;
+        this.webduino.ws2812 = null;
+      }
+      return this.showBoardSetting;
+    },
+    executeCode() {
+      if (!this.input.deviceID || !this.input.showBoardSetting) {
+        return;
+      }
+
+      const config = {
+        board: this.input.deviceType,
+        device: this.input.deviceID,
+        transport: 'mqtt'
+      };
+
+      if (this.webduino.queue) {
+        clearTimeout(this.webduino.queue);
+      }
+      this.webduino.queue = setTimeout(() => {
+        if (this.webduino.ws2812) {
+          return this.webduino.ws2812.setColor(this.exportedCode);
+        }
+        this.createBoard(config).then((board) => {
+          board.samplingInterval = 50;
+          board.once(webduino.BoardEvent.DISCONNECT, () => {
+            this.webduino.ws2812 = null;
+          });
+          this.webduino.ws2812 = getWS2812(board, this.input.pin, this.leds.length);
+          this.webduino.ws2812.setColor(this.exportedCode);
+        })
+        .catch((ex) => {
+          alert(ex);
+        });
+      }, 800);
     }
   }
 });
